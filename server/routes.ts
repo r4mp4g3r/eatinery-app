f13fb143258dc198e041a16ad83fe051b480da52
Pachigulla Ramtej <ram.pachi.tej@gmail.com>
2025-04-18 04:25:25 +0800

    Update routes.ts

--- server/routes.ts
+++ server/routes.ts
@@ -1,8 +1,8 @@
 import type { Express } from "express";
 import { createServer, type Server } from "http";
-import { storage } from "./storage";
+import { storage } from "./storage.js";
 import { calorieFilterSchema } from "@shared/schema";
-import { setupAuth } from "./auth";
+import { setupAuth } from "./auth.js";
 
 export async function registerRoutes(app: Express): Promise<Server> {
   // Setup authentication routes
