baa29252d57faca1ad836df105369916d4ca0196
Pachigulla Ramtej <ram.pachi.tej@gmail.com>
2025-04-18 04:24:58 +0800

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,7 @@
 import express, { type Request, Response, NextFunction } from "express";
-import { registerRoutes } from "./routes";
-import { setupVite, serveStatic, log } from "./vite";
-import { storage } from "./storage";
+import { registerRoutes } from "./routes.js";
+import { setupVite, serveStatic, log } from "./vite.js";
+import { storage } from "./storage.js";
 
 const app = express();
 app.use(express.json());
